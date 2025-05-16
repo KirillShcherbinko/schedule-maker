ac80819cd0cd8d6f71713c698ce8bdbb9f7a073b
Kirill Shcherbinko <kirill.sherbinko@gmail.com>
2025-05-16 18:46:18 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: false,
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: "build",
+    sourcemap: true,
+  },
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
