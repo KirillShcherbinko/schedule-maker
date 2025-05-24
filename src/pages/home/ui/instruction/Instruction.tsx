73a0ce3ae4af50ef1533a842d1fb3e39f9e9de00
Kirill Shcherbinko <kirill.sherbinko@gmail.com>
2025-05-24 22:16:32 +0300

    refactor(pages): replace budge with tooltip in a instruction component to accordion badge

--- src/pages/home/ui/instruction/Instruction.tsx
+++ src/pages/home/ui/instruction/Instruction.tsx
@@ -1,7 +1,9 @@
+import SchedulingGuy from './assets/scheduling-guy.png';
 import { useTranslation } from 'react-i18next';
 import { BASE_NAMESPACE } from '../../model/consts';
 import type { TBadgeColor } from '@/shared/ui/Badge/model/types';
-import { BadgeWithTooltip } from './ui/badge-with-tooltip';
+import { AccordionBadge } from './ui/accordion-badge';
+import { Accordion } from '@/shared/ui/Accordion';
 
 type TBadgeWithTooltip = {
   title: string;
@@ -16,12 +18,15 @@ export const Instruction = () => {
   const steps = t(`${BASE_LINK}.steps`, { returnObjects: true, ns: 'home' }) as TBadgeWithTooltip[];
 
   return (
-    <section className="flex flex-col justify-center items-center gap-10 bg-block-primary p-8 rounded-3xl">
+    <section className="flex flex-col justify-center items-center bg-block-primary p-8 rounded-3xl w-full">
       <h2 className="font-bold text-3xl text-center">{t(`${BASE_LINK}.title`, BASE_NAMESPACE)}</h2>
-      <div className="flex flex-wrap gap-5 justify-center items-center">
-        {steps.map((step, index) => (
-          <BadgeWithTooltip badge={{ title: step.title, color: colors[index] }} content={step.text} />
-        ))}
+      <div className="flex flex-col items-center lg:flex-row gap-2 w-full">
+        <img className="w-lg" src={SchedulingGuy} alt="scheduling-guy" />
+        <Accordion type="multiple" className="flex flex-col gap-5 justify-center w-full">
+          {steps.map((step, index) => (
+            <AccordionBadge badge={{ title: step.title, color: colors[index] }} content={step.text} />
+          ))}
+        </Accordion>
       </div>
     </section>
   );
--- src/pages/home/ui/instruction/ui/badge-with-tooltip/index.ts
+++
@@ -1,1 +0,0 @@
-export { BadgeWithTooltip } from './BadgeWithTooltip';
